68fb90643805d813456b76d167b41e4c4b111505
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-06-24 04:44:02 -0300

    component SubTitleTopBody

---
+++ src/components/SubTitleTopBody/SubTitleTopBody.js
@@ -0,0 +1,12 @@
+import { SubTitleTBody, WrapperSubTitleTBody } from './SubTitleTopBody.styles';
+
+export default function SubTitleTopBody() {
+
+    return (
+        <WrapperSubTitleTBody>
+            <SubTitleTBody className='montserrat-medium-500-16'>
+                Ganhe R$ 10,00 de desconto no frete
+            </SubTitleTBody>
+        </WrapperSubTitleTBody>
+    )
+};
