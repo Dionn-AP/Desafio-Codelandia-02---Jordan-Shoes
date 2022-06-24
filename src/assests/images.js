59d6855d5e52d48b739d3b4a896320c1f7675d1d
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-06-24 04:39:10 -0300

    arquivo de imagens styled

---
+++ src/assests/images.js
@@ -0,0 +1,45 @@
+import image01 from '../assests/1.png';
+import image02 from '../assests/2.png';
+import image03 from '../assests/3.png';
+import image04 from '../assests/4.png';
+import image05 from '../assests/5.png';
+import image06 from '../assests/6.png';
+import image07 from '../assests/7.png';
+import image08 from '../assests/8.png';
+
+const allImagesShoes = [
+    {
+        id: 1,
+        image: image01
+    },
+    {
+        id: 2,
+        image: image02
+    },
+    {
+        id: 3,
+        image: image03
+    },
+    {
+        id: 4,
+        image: image04
+    },
+    {
+        id: 5,
+        image: image05
+    },
+    {
+        id: 6,
+        image: image06
+    },
+    {
+        id: 7,
+        image: image07
+    },
+    {
+        id: 8,
+        image: image08
+    },
+];
+
+export default allImagesShoes;
