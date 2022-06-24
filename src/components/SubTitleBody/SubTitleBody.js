19fed3818b335b7c1d2a4adf8658e231030f648e
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-06-24 04:43:34 -0300

    component SubTitleBody

---
+++ src/components/SubTitleBody/SubTitleBody.js
@@ -0,0 +1,10 @@
+import { SubTitleBodyContent } from './SubTitleBody.styles';
+
+export default function SubTitleBody() {
+
+    return (
+        <SubTitleBodyContent className='montserrat-medium-500-18'>
+            Frete grátis e chinelo de brinde é aqui, aproveite por tempo limitado.
+        </SubTitleBodyContent>
+    )
+};
