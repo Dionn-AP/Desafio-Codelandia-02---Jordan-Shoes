458dda95507a3bb090ba66509c23feb5090bb455
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-06-24 04:43:12 -0300

    component Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import { FooterMain } from './Footer.styles';
+
+export default function Footer() {
+
+    return (
+        <FooterMain>
+            Todos os direitos reservados.
+        </FooterMain>
+    )
+}
