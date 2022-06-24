0510afae16dd98ff756c7d03b305a52ebd57b883
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-06-24 12:00:38 -0300

    cofigurando o scrollReveal

--- src/components/CardShoes/CardShoes.js
+++ src/components/CardShoes/CardShoes.js
@@ -1,17 +1,35 @@
 import { CardShoe, ImageShoe } from './CardShoees.styles';
 import allImagesShoes from '../../assests/images';
+import ScrollReveal from 'scrollreveal';
+import { useEffect } from 'react';
 
 export default function CardShoes() {
+
+    useEffect(() => {
+        const srTop = ScrollReveal({
+            distance: '100px',
+            duration: 1500,
+            reset: true
+        });
+        const srBottom = ScrollReveal({
+            scale: 0.75,
+            distance: '30px',
+            duration: 1500,
+            reset: true
+        });
+        srTop.reveal('#cards-shoe', { interval: 180, origin: 'top' });
+        srBottom.reveal('#img-shoe',  { interval: 200 });
+    }, [])
 
     return (
         <>
             {
                 allImagesShoes.map((image) => (
-                    <CardShoe key={image.id}>
-                        <ImageShoe 
-                        className='images-shoes'
-                        src={image.image} 
-                        alt='shoes' />
+                    <CardShoe key={image.id} id='cards-shoe'>
+                        <ImageShoe
+                            id='img-shoe'
+                            src={image.image}
+                            alt='shoes' />
                     </CardShoe>
                 ))
             }
