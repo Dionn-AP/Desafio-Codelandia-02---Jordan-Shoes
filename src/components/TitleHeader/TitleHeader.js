bb1798fb4265a8c7cacdf0a1095dadfbce8e6aa4
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-06-24 04:45:00 -0300

    component TitleHeader

---
+++ src/components/TitleHeader/TitleHeader.js
@@ -0,0 +1,10 @@
+import { TitleHeaderContent } from './TitleHeader.styles';
+
+export default function TitleHeader() {
+
+    return (
+        <TitleHeaderContent className='montserrat-bold-700-32'>
+            A melhor loja de Jordan
+        </TitleHeaderContent>
+    )
+}
