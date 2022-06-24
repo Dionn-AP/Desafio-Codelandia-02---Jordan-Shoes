467e547c1fedf2e57b0e051e29b731ac62624f9d
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-06-24 04:40:29 -0300

    styles do component Footer

---
+++ src/components/Footer/Footer.styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const FooterMain = styled.div`
+    display: flex;
+    width: 100%;
+    height: 6rem;
+    align-items: center;
+    justify-content: center;
+    color: var(--bgSite);
+    background-color: var(--title);
+`
