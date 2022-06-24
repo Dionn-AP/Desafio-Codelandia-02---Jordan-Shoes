50a19eceee169ed8ceb6d4ba2d47640a0dae099e
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-06-24 04:42:40 -0300

    styles do component TitleTopBody

---
+++ src/components/TitleTopBody/TitleTopBody.styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const WrapperTitleTBody = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 1rem 0;
+    width: 100%;
+    text-align: center;
+    background-color: var(--bgSite);
+`
+
+export const TitleTBody = styled.h4`
+    color: var(--title);
+`
