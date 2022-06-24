3a75b5a38b7bc71a8f00fb21fb811545bded8148
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-06-24 04:41:55 -0300

    styles do component SubTitleTopBody

---
+++ src/components/SubTitleTopBody/SubTitleTopBody.styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const WrapperSubTitleTBody = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 1.5rem 0;
+    width: 100%;
+    background-color: var(--title);
+`
+
+export const SubTitleTBody = styled.h4`
+    color: var(--bgSite);
+`
