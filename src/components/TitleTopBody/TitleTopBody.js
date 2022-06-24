8f21cb2aa335a753f65d706810ab9c0709bf2b7b
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-06-24 04:45:13 -0300

    component TitleTopBody

---
+++ src/components/TitleTopBody/TitleTopBody.js
@@ -0,0 +1,12 @@
+import { WrapperTitleTBody, TitleTBody } from './TitleTopBody.styles';
+
+export default function TitleTopBody() {
+
+    return (
+        <WrapperTitleTBody>
+            <TitleTBody className='montserrat-semi-bold-600-24'>
+                JordanShoes
+            </TitleTBody>
+        </WrapperTitleTBody>
+    )
+};
