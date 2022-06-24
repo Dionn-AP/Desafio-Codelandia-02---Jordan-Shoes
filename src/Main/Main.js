43e23975bdc42401c73454e395d3b1df21eb0f91
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-06-24 04:36:45 -0300

    arquivo Main

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,9 +1,29 @@
-import './App.css';
+import CardShoes from '../components/CardShoes/CardShoes';
+import Header from '../components/Header/Header';
+import SubTitleBody from '../components/SubTitleBody/SubTitleBody';
+import SubTitleTopBody from '../components/SubTitleTopBody/SubTitleTopBody';
+import TitleBody from '../components/TitleBody/TitleBody';
+import TitleTopBody from '../components/TitleTopBody/TitleTopBody';
+import Footer from '../components/Footer/Footer';
+import { WrapperCardsShoes, WrapperContentMain, TitleBodyContent } from '../global/Styles';
+import './main.css';
 
 function Main() {
   return (
     <div className="container">
-      
+      <SubTitleTopBody />
+      <TitleTopBody />
+      <Header />
+      <WrapperContentMain>
+        <TitleBodyContent>
+          <TitleBody />
+          <SubTitleBody />
+        </TitleBodyContent>
+        <WrapperCardsShoes>
+          <CardShoes />
+        </WrapperCardsShoes>
+      </WrapperContentMain>
+      <Footer />
     </div>
   );
 }
