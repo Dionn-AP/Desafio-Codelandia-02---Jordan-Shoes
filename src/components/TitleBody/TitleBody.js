ca34622fcc9987e8c74d7014dd3b8e90b5a3ed6c
Dionnatan Alves <dionn_a.p@hotmail.com>
2022-06-24 04:44:46 -0300

    component TitleBody

---
+++ src/components/TitleBody/TitleBody.js
@@ -0,0 +1,10 @@
+import { TitleBodyContent } from './TitleBody.styles';
+
+export default function TitleBody() {
+
+    return (
+        <TitleBodyContent className='montserrat-semi-bold-600-32'>
+            Destaques
+        </TitleBodyContent>
+    )
+}
